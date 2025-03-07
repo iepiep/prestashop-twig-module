22d0974b8bb4b7c773837cf4eccf669650ac86d7
Roberto Minini <51701774+iepiep@users.noreply.github.com>
2025-03-07 13:22:14 +0100

    Update ItineraryService.php

--- src/Service/ItineraryService.php
+++ src/Service/ItineraryService.php
@@ -5,8 +5,11 @@
  * @copyright 2025 Roberto Minini
  * @license MIT
  */
-
 namespace DimSymfony\Service;
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
 
 use PrestaShop\PrestaShop\Adapter\LegacyContext;
 
