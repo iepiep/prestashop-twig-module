279cc6b39342d08295c7e0c62732b8472220d88a
Roberto Minini <51701774+iepiep@users.noreply.github.com>
2025-03-07 15:58:24 +0100

    Update OtherPageController.php

--- src/Controller/Admin/OtherPageController.php
+++ src/Controller/Admin/OtherPageController.php
@@ -1,14 +1,28 @@
 <?php
 
-namespace Dimsymfony\Controller\Admin; // Corrected Namespace
+namespace Dimsymfony\Controller\Admin;
 
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use Symfony\Component\HttpFoundation\Response;
+use Dimsymfony\Entity\CustomerItinerary; // Import the entity
+use Doctrine\ORM\EntityManagerInterface;
 
 class OtherPageController extends FrameworkBundleAdminController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
     public function indexAction(): Response
     {
-        return $this->render('@Modules/dimsymfony/views/templates/admin/other_page.html.twig');
+        // Fetch all customer itinerary data
+        $repository = $this->entityManager->getRepository(CustomerItinerary::class);
+        $customerData = $repository->findAll();
+
+        return $this->render('@Modules/dimsymfony/views/templates/admin/other_page.html.twig', [
+            'customerData' => $customerData,
+        ]);
     }
 }
