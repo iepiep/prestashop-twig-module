dddbd13db27df75c4bfb10b87f0a59e59c0ffa30
Roberto Minini <51701774+iepiep@users.noreply.github.com>
2025-03-07 13:26:28 +0100

    Update dimsymfony.php

--- dimsymfony.php
+++ dimsymfony.php
@@ -1,5 +1,13 @@
-
 <?php
+/**
+ * @author Roberto Minini <r.minini@solution61.fr>
+ * @copyright 2025 Roberto Minini
+ * @license MIT
+ *
+ * This file is part of the dimrdv project.
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 declare(strict_types=1);
 
 if (!defined('_PS_VERSION_')) {
